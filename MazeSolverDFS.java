7b20a5fde16e400061046c71470fb9b1c576d4e8
martinn8145 <69876923+martinn8145@users.noreply.github.com>
2020-11-16 03:57:50 -0800

    Add files via upload
    Haven't tested maze solver, but logically it should work. Need to implement mazeEditor() to use for both BFS and DFS but it shouldn't be too hard

---
+++ MazeSolverDFS.java
@@ -0,0 +1,19 @@
+package nguyen.cs146project3;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class MazeSolverDFS {
+	
+	public String mazeSolver(LinkedList<Cell>[] adjacencyList, String maze, Cell startingCell) {
+		int counter = 0;
+		Stack<Cell> cellStack = new Stack<Cell>();
+		int arrayIndex = 0, listIndex = 0;
+		
+		return maze;
+	}
+	
+	public String mazeEditHelper(String maze, int row, int column, int number) {
+		return maze;
+	}
+}

79c263642975fc8f1eb6b818555374f0d7cba132
martinn8145 <69876923+martinn8145@users.noreply.github.com>
2020-11-16 15:42:37 -0800

    Update MazeSolverDFS.java
    mazeEditor implemented

--- MazeSolverDFS.java
+++ MazeSolverDFS.java
@@ -13,7 +13,22 @@ public class MazeSolverDFS {
 		return maze;
 	}
 	
-	public String mazeEditHelper(String maze, int row, int column, int number) {
-		return maze;
+	//edits the maze to show the path of the DFS solver 
+	public String mazeEditor(String maze, int row, int column, int number) {
+		// find the distance to the new line character and add 1 to get to the start of a row
+		int line = maze.indexOf("\n") + 1;
+		// to get to the specified row
+		int rowIndex = line + (line * 2 * row);
+		// to get to the specified column
+		int colIndex =  1 + (column*2);
+		// combine to get the index of the cell
+		int cellIndex = rowIndex + colIndex;
+		// keep the maze the same until the cell
+		String newMaze = maze.substring(0, cellIndex);
+		// add the number
+		newMaze += number;
+		// add the rest of the maze 
+		newMaze += maze.substring(cellIndex + 1);
+		return newMaze;
 	}
 }

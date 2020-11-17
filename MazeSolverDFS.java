bc64dc6b420f5f9ebbdb69435d2a53f93fda77b7
martinn8145 <69876923+martinn8145@users.noreply.github.com>
2020-11-16 17:40:48 -0800

    Update MazeSolverDFS.java

--- MazeSolverDFS.java
+++ MazeSolverDFS.java
@@ -3,32 +3,65 @@ package nguyen.cs146project3;
 import java.util.LinkedList;
 import java.util.Stack;
 
-public class MazeSolverDFS {
-	
-	public String mazeSolver(LinkedList<Cell>[] adjacencyList, String maze, Cell startingCell) {
-		int counter = 0;
-		Stack<Cell> cellStack = new Stack<Cell>();
-		int arrayIndex = 0, listIndex = 0;
+public class MazeSolverDFS extends MazeHelper {
+	private boolean[][] visited; //boolean array to check if the cell's been visited
+	/**
+	 * using an adjacency list representation of a graph, edit the maze to show 
+	 * DFS approach to solving the maze
+	 * @param graph graph of all of the cells
+	 * @param maze the unsolved maze as a string
+	 * @param startingCell the cell we start from (0,0)
+	 * @return the path DFS algorithm takes to solve the maze
+	 */
+	public String mazeSolver(Graph graph, String maze, Cell startingCell) {
 		
+		graph.addEdge();
+		LinkedList<Cell>[] adjacencyList = graph.getArrayOfLinked();
+		int len = (int) Math.sqrt(adjacencyList.length);
+		int counter = 0;// the number being printed in the maze 
+		
+		visited = new boolean[len][len];
+		Stack<Cell> cellStack = new Stack<Cell>();
+		
+		cellStack.push(startingCell);
+		visited[startingCell.getRow()][startingCell.getColumn()] = true;
+		
+		while (!cellStack.isEmpty()) {
+			Cell currentCell = cellStack.pop();
+			int row = currentCell.getRow(), col = currentCell.getColumn();
+			maze = mazeEditor(maze, row, col, counter++);
+			//end of the maze means break
+			if (row == len - 1 && col == len - 1) {
+				break;
+			}
+			//edit the maze to show the current step
+			if (counter ==10) counter = 0; // only use single digit numbers
+			LinkedList<Cell> neighborList = graph.getList(row, col); 	//get the entire adjacency list for a cell
+			
+			for (Cell neighbor: neighborList) {		//iterate through to find unvisited cells
+				if (!visited[neighbor.getRow()][neighbor.getColumn()])  {
+					cellStack.push(neighbor);		//push the cells onto the stack and mark them as visited
+					visited[neighbor.getRow()][neighbor.getColumn()] = true;
+				}
+			}
+		}
 		return maze;
 	}
-	
-	//edits the maze to show the path of the DFS solver 
-	public String mazeEditor(String maze, int row, int column, int number) {
-		// find the distance to the new line character and add 1 to get to the start of a row
-		int line = maze.indexOf("\n") + 1;
-		// to get to the specified row
-		int rowIndex = line + (line * 2 * row);
-		// to get to the specified column
-		int colIndex =  1 + (column*2);
-		// combine to get the index of the cell
-		int cellIndex = rowIndex + colIndex;
-		// keep the maze the same until the cell
-		String newMaze = maze.substring(0, cellIndex);
-		// add the number
-		newMaze += number;
-		// add the rest of the maze 
-		newMaze += maze.substring(cellIndex + 1);
-		return newMaze;
+
+	public static void main (String[] args) {
+		MazeGenerator jeff = new MazeGenerator(4);
+		jeff.cellGenerator();
+		MazePrinter mp = new MazePrinter();
+		Cell[][] cellArray = mp.listTo2DArray(jeff.getCellList());
+		String maze = mp.printMaze(cellArray);
+		Graph graph = new Graph(jeff.getCellList());
+		//System.out.println(maze);
+		MazeSolverDFS ms = new MazeSolverDFS();
+		Cell startingCell = cellArray[0][0];
+		System.out.print(ms.mazeSolver(graph, maze, startingCell));
+		//maze = ms.mazeEditor(maze, 0, 0, 0);
+		//System.out.println(maze);
+		//System.out.println(ms.mazeEditor(maze, 5, 5, 1));
+		
 	}
 }

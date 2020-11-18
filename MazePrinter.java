a074b8caf5e2c2613c2e6cdf6608f97438926a7a
martinn8145 <69876923+martinn8145@users.noreply.github.com>
2020-11-17 17:05:37 -0800

    Update MazePrinter.java

--- MazePrinter.java
+++ MazePrinter.java
@@ -1,44 +1,47 @@
 package nguyen.cs146project3;
 
-import java.util.ArrayList;
+public class MazePrinter extends MazeHelper {
 
-public class MazePrinter {
-	public static String printMaze(Cell[][] cellArray) {
+	// returns a string of the empty maze created by the MazeGenerator class
+	public String printMaze(Cell[][] cellArray) {
 		String maze = "";
-		//first line
+		// first line looks like + +-+-+-+...
 		for (int firstLine = 0; firstLine < cellArray.length; firstLine++) {
-			if (firstLine !=0) {
+			if (firstLine != 0) {
 				maze += "-+";
 			} else {
 				maze += "+ +";
 			}
 		}
 		maze += "\n";
-		for (int i = 0; i < cellArray.length; i++) {		
-				maze += "|";
-				for (int j = 0; j < cellArray.length; j++) {
-					Cell currentCell = cellArray[i][j];
-					maze += " ";
-					if (currentCell.get("East")) {
-						maze += "|";
-					} else maze += " ";
+		// move on to the body of the maze
+		for (int i = 0; i < cellArray.length; i++) {
+			maze += "|"; // left most wall always exists
+			for (int j = 0; j < cellArray.length; j++) {
+				Cell currentCell = cellArray[i][j]; // get the current cell and add a space
+				maze += " ";
+				if (currentCell.get("East")) { // if there's a wall to the right then add a wall
+					maze += "|";
+				} else
+					maze += " "; // if there's not then add a space
+			}
+			maze += "\n";
+			// add horizontal walls before last line
+			if (i != cellArray.length - 1) {
+				maze += "+"; // left side always start with +
+				for (int k = 0; k < cellArray.length; k++) {
+					Cell currentCell = cellArray[i][k];
+					if (currentCell.get("South")) { // check if the bottom wall is intact
+						maze += "-";
+					} else {
+						maze += " ";
+					}
+					maze += "+"; // right side always ends in +
 				}
 				maze += "\n";
-				
-				if (i != cellArray.length - 1) {
-					maze += "+";
-					for (int k = 0; k < cellArray.length; k++) {
-						Cell currentCell = cellArray[i][k];
-						if (currentCell.get("South")) {
-							maze += "-";
-						} else {
-							maze += " ";
-						}
-						maze += "+";
-					}
-					maze += "\n";
-				}			
+			}
 		}
+		// last line looks like this ... +-+-+ +
 		for (int lastLine = 0; lastLine < cellArray.length; lastLine++) {
 			if (lastLine != cellArray.length - 1) {
 				maze += "+-";
@@ -47,12 +50,5 @@ public class MazePrinter {
 			}
 		}
 		return maze;
-	}
-	
-	public static void main (String[] args) {
-		Generator jeff = new Generator(45);
-		jeff.cellGenerator();
-		Cell[][] cellArray = listTo2DArray(jeff.getCellList());
-		System.out.print(printMaze(cellArray));
 	}
 }

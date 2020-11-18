cc69069edc6525155f23c66b8a450d202893a61d
martinn8145 <69876923+martinn8145@users.noreply.github.com>
2020-11-17 17:04:00 -0800

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,18 +1,22 @@
 package nguyen.cs146project3;
 
-
 import java.util.HashMap;
+
 /**
  * 
- * @author martinnguyen
- * each cell will contain a hashmap of walls which will be represented by Strings
- * the walls will serve as keys, which contain boolean values indicating whether or not the wall is broken
- * all cells will be initialized with intact walls on all 4 sides
+ * @author martinnguyen each cell will contain a hashmap of walls which will be
+ *         represented by Strings the walls will serve as keys, which contain
+ *         boolean values indicating whether or not the wall is broken all cells
+ *         will be initialized with intact walls on all 4 sides
  */
-public class Cell extends HashMap<String,Boolean> implements Comparable<Cell>{
+public class Cell extends HashMap<String, Boolean> implements Comparable<Cell> {
+
 	private int rowPosition;
 	private int columnPosition;
-	
+
+	Cell parentCell; // for BFS, have each cell have a parentCell unless its the first cell; useful
+						// for backtracking
+
 	public Cell(int rowPosition, int columnPosition) {
 		this.rowPosition = rowPosition;
 		this.columnPosition = columnPosition;
@@ -24,47 +28,39 @@ public class Cell extends HashMap<String,Boolean> implements Comparable<Cell>{
 		this.put("South", true); // bottom wall
 		this.put("East", true); // right wall
 	}
-	
+
 	public int getRow() {
 		return this.rowPosition;
 	}
+
 	public int getColumn() {
 		return this.columnPosition;
 	}
-	
-	public boolean deepEquals(Object x) {
-		Cell that = (Cell) x;
-		if (this.rowPosition == that.rowPosition && this.columnPosition == that.columnPosition) {
-			if (this.get("North") == that.get("North") && this.get("South") == that.get("South")) {
-				if (this.get("West") == that.get("West") && this.get("East") == that.get("East")) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+
 	public boolean equals(Object x) {
 		Cell that = (Cell) x;
 		return this.rowPosition == that.rowPosition && this.columnPosition == that.columnPosition;
 	}
+
 	public int hashCode() {
 		return this.hashCode();
 	}
+
 	// checks if all walls are intact
 	public boolean allWallsIntact() {
 		return this.get("North") && this.get("South") && this.get("West") && this.get("East");
 	}
-	
-	// implements comparable
+
+	// use compareTo to order arraylist in graph class
 	public int compareTo(Cell compareCell) {
-	        int compareRow = compareCell.getRow();
-	        int compareCol = compareCell.getColumn();
-	        int thisRow = this.getRow();
-	        int thisCol = this.getColumn();
-	        int rowDiff = Integer.compare(thisRow,compareRow);
-	        if (rowDiff == 0) {
-	        	return Integer.compare(thisCol, compareCol);
-	        }
-	        return rowDiff;
-	 }
+		int compareRow = compareCell.getRow();
+		int compareCol = compareCell.getColumn();
+		int thisRow = this.getRow();
+		int thisCol = this.getColumn();
+		int rowDiff = Integer.compare(thisRow, compareRow);
+		if (rowDiff == 0) {
+			return Integer.compare(thisCol, compareCol);
+		}
+		return rowDiff;
+	}
 }
